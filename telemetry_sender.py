5ed4f3845b427e34900836bc2ad4f3880dafd26b
Ethan <ethantjong@gmail.com>
2026-06-26 10:03:47 -0500

    Update telemetry_sender.py

--- telemetry_sender.py
+++ telemetry_sender.py
@@ -449,7 +449,7 @@ def build_parser():
                         help="MPPT: PV voltage change to trigger transmit (V)")
     parser.add_argument("--mppt-pv-current-delta-a", type=float, default=0.5,
                         help="MPPT: PV current change to trigger transmit (A)")
-    parser.add_argument("--mppt-pv-power-delta-w", type=float, default=10.0,
+    parser.add_argument("--mppt-pv-power-delta-w", type=float, default=1.0,
                         help="MPPT: PV power change to trigger transmit (W)")
     parser.add_argument("--mppt-batt-voltage-delta-v", type=float, default=1.0,
                         help="MPPT: battery voltage change to trigger transmit (V)")
